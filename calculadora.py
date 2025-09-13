ee3fb3a61e06cc4c2c89e8a72eec71b613f398b9
rochaheber86 <rocha.heber86@gmail.com>
2025-09-12 21:22:53 -0300

    Criação da calculadora
    Etapa 1 - Criar calculadora

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+# PUC-PR Atividade Formativa ADS
+# Aluno Heber Leite da Rocha
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Erro!!"
+
+print("PUC PR - Calculadora ADS")
