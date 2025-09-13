dd67eade07a97a0b89027dae9b1b3106ca5c8ff4
rochaheber86 <rocha.heber86@gmail.com>
2025-09-12 21:26:21 -0300

    Permite usuário escolher no Menu
    Etapa 3 - Escolha no menu da aplicação

--- calculadora.py
+++ calculadora.py
@@ -17,9 +17,17 @@ def divide(x, y):
         return "Erro!!"
 
 #Criação do menu
-print("PUC PR - Calculadora ADS - Com menu")
 print("Selecione a operação:")
 print("1.Somar")
 print("2.Subtrair")
 print("3.Multiplicar")
 print("4.Dividir")
+
+
+#Permite usuário escolher
+escolha = input("Digite sua opção: ")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print("PUC PR - Calculadora ADS - Com menu")
+print(f"Escolha: {escolha}, N1: {num1}, N2: {num2}")
