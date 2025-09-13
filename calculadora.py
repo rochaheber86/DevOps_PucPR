854d0f5234d4b2f88b592341dc3f294cf8ecbeae
rochaheber86 <rocha.heber86@gmail.com>
2025-09-12 21:27:55 -0300

    Adiciona Operações + escolha

--- calculadora.py
+++ calculadora.py
@@ -31,3 +31,18 @@ num2 = float(input("Digite o segundo número: "))
 
 print("PUC PR - Calculadora ADS - Com menu")
 print(f"Escolha: {escolha}, N1: {num1}, N2: {num2}")
+
+
+#Escolha com operações
+if escolha == '1':
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+elif escolha == '2':
+    print(f"{num1} - {num2} = {subtract(num1, num2)}")
+elif escolha == '3':
+    print(f"{num1} * {num2} = {multiply(num1, num2)}")
+elif escolha == '4':
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+    print("Erro!")
+
+
