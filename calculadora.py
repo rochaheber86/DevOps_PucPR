88ff8ba381955f2a937a1c5a1953ca0f259fd878
rochaheber86 <rocha.heber86@gmail.com>
2025-09-12 21:58:42 -0300

    Ajuste de erros encontrados nos testes

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,18 @@
 # PUC-PR Atividade Formativa ADS
 # Aluno Heber Leite da Rocha
 
+
 def add(x, y):
     return x + y
+
 
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x * y
+
 
 def divide(x, y):
     if y != 0:
@@ -16,7 +20,8 @@ def divide(x, y):
     else:
         return "Erro!!"
 
-#Criação do menu
+
+# Criação do menu
 print("Selecione a operação:")
 print("1.Somar")
 print("2.Subtrair")
@@ -24,7 +29,7 @@ print("3.Multiplicar")
 print("4.Dividir")
 
 
-#Permite usuário escolher
+# Permite usuário escolher
 escolha = input("Digite sua opção: ")
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
@@ -33,7 +38,7 @@ print("PUC PR - Calculadora ADS - Com menu")
 print(f"Escolha: {escolha}, N1: {num1}, N2: {num2}")
 
 
-#Escolha com operações
+# Escolha com operações
 if escolha == '1':
     print(f"{num1} + {num2} = {add(num1, num2)}")
 elif escolha == '2':
@@ -50,6 +55,3 @@ while True:
     again = input("Deseja fazer outro calculo?: ")
     if again.lower() != 's':
         break
-
-
-
